1d9b04fe4117a02f25fd652ed59085958b7e1db6
koybigino <koybigino@gmail.com>
2022-09-03 02:16:19 +0100

    Connection with postgres database usiing Gorm ORM

--- go.mod
+++ go.mod
@@ -4,22 +4,27 @@ go 1.18
 
 require (
 	github.com/gofiber/fiber/v2 v2.36.0
+	gorm.io/driver/postgres v1.3.9
 	gorm.io/gorm v1.23.8
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/fatih/color v1.9.0 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/githubnemo/CompileDaemon v1.4.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jackc/pgx/v4 v4.16.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
-	github.com/mattn/go-colorable v0.1.4 // indirect
-	github.com/mattn/go-isatty v0.0.11 // indirect
-	github.com/radovskyb/watcher v1.0.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.38.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"math/rand"
+	"os"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Post struct {
-	Id        int    `json:"id"`
+	Id        int    `json:"id" gorm:"primaryKey"`
 	Title     string `json:"title"`
 	Content   string `json:"content"`
 	Published bool   `json:"published"`
@@ -25,24 +28,47 @@ var (
 func main() {
 	app := fiber.New()
 
+	dsn := "host=localhost user=postgres password=Bielem@*01 dbname=fiber_bd port=5432 sslmode=disable TimeZone=UTC"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Printf("Error to connect to our databse : %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("connection to the database ok .... %v", db)
+
 	app.Get("/posts", func(c *fiber.Ctx) error {
-		return c.JSON(postList)
+		var post []Post
+		result := db.Find(&post)
+		if result.Error != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"message": "Error to get all elements of the table",
+			})
+		}
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "All post are getting well",
+			"data":    post,
+		})
 	})
 
 	app.Get("/posts/:id", func(c *fiber.Ctx) error {
-		getPost := new(Post)
-		for _, post := range postList {
-			if strconv.Itoa(post.Id) == c.Params("id") {
-				getPost = &post
-				break
-			}
-		}
-		if getPost.Title == "" {
+		id := c.Params("id")
+		post := new(Post)
+		intId, err := strconv.Atoi(id)
+		if err != nil {
 			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-				"message": "Post not found !",
+				"message": "Please enter a integer like parameter !",
 			})
 		}
-		return c.JSON(getPost)
+		e := db.First(post, intId).Error
+		if e != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"message": "post not found !",
+				"error":   e,
+			})
+		}
+		return c.JSON(post)
 	})
 
 	app.Post("/posts", func(c *fiber.Ctx) error {
@@ -53,28 +79,30 @@ func main() {
 				"message": err.Error(),
 			})
 		}
-		newPost.Id = rand.Intn(1000)
-		postList = append(postList, *newPost)
-		return c.JSON(postList)
+		err := db.Create(newPost).Error
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "Your post wasn't been created !",
+				"error":   err,
+			})
+		}
+		return c.JSON(fiber.Map{
+			"message": "Your post was been created !",
+			"data":    newPost,
+		})
 	})
 
 	app.Delete("/posts/:id", func(c *fiber.Ctx) error {
 		id := c.Params("id")
+		post := new(Post)
 		intId, err := strconv.Atoi(id)
 		if err != nil {
 			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 				"message": "Please enter a integer like parameter !",
 			})
 		}
-		for index, post := range postList {
-			if post.Id == intId {
-				postList = append(postList[:index], postList[index+1:]...)
-				return c.SendStatus(fiber.StatusNoContent)
-			}
-		}
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"message": "Post not found to delete it",
-		})
+		db.Delete(post, intId)
+		return c.SendStatus(fiber.StatusNoContent)
 	})
 
 	app.Put("/posts/:id", func(c *fiber.Ctx) error {
@@ -87,23 +115,18 @@ func main() {
 			})
 		}
 
-		for index, post := range postList {
-			if intId == post.Id {
-				if err := c.BodyParser(newPost); err != nil {
-					return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-						"message": err.Error(),
-					})
-				}
-				postList = append(postList[:index], postList[index+1:]...)
-				newPost.Id = intId
-				postList = append(postList, *newPost)
-				c.SendStatus(fiber.StatusCreated)
-				return c.JSON(postList)
-			}
+		e := db.First(newPost, intId)
+		if e.Error != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"message": "post not found !",
+			})
 		}
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"message": "Post not found to update it !",
-		})
+
+		newPost.Title = "New Title update"
+		newPost.Content = "New Content update"
+		db.Save(newPost)
+
+		return c.JSON(newPost)
 
 	})
 
